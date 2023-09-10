8d5388feb1c47bbe8f5659a540f4b2033740057a
Javohir Odilbekov <odilbekovjavohir4@gmail.com>
2023-09-10 18:40:45 +0500

    init

---
+++ begin11/index.php
@@ -0,0 +1,10 @@
+<?php
+$a = 3;
+$b = 4;
+$c = $a+$b;
+$d = $a*$b;
+$m_a = abs($a);
+$m_b = abs($b);
+echo" $c $d $m_a $m_b"
+
+?>
---
+++ begin9/index.php
@@ -0,0 +1,6 @@
+<?php
+$a = abs(-34);
+$b = abs(-34);
+
+echo "Answer: ".sqrt($a*$b);
+?>
