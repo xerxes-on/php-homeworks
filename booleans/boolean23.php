6ae05b0f9492087dd8500cbe847e3f11b2e9774b
Javokhir18 <122219058+Javokhir18@users.noreply.github.com>
2023-09-15 16:15:29 +0500

    init

---
+++ booleans/boolean23.php
@@ -0,0 +1,10 @@
+
+<?php
+
+$a = 201;
+$b= $a%10;
+$c = floor($a/100);
+$d = floor(($a - floor($a/100)*100)/10);
+echo var_dump($a==$c ); 
+
+?>
---
+++ booleans/boolean33.php
@@ -0,0 +1,9 @@
+<?php
+
+$a=3;
+$b=10;
+$c=10;
+echo var_export($a<$b+$c&&$b<$a+$c&&$c<$b+$c);
+
+
+?>
