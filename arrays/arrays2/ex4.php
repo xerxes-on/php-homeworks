3363592f5cc4279bee931fd3c63521b5eb15aa87
Javokhir18 <122219058+Javokhir18@users.noreply.github.com>
2023-10-09 06:58:39 +0500

    added

--- arrays/arrays2/ex4.php
+++ arrays/arrays2/ex4.php
@@ -1,1 +1,17 @@
 <?php
+$n = 1990;
+$num = [];
+for($i =1; $i<$n; $i++){
+    $div = 0;
+    for($j = 1; $j < $i; $j++){
+       if($i % $j == 0 ){
+        $div += $j;
+    }
+    }
+    if($i == $div && !($i % 4 ==0)){
+        array_push($num, $i);
+    }
+}
+var_dump($num);
+
+?>
